1784b3f2c4902e3fc5b1b26f411340dfb953a820
Ashleyduffy <47986827+Ashleyduffy@users.noreply.github.com>
2020-08-12 15:01:42 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,12 @@
+//open nav
+ $(document).ready(function() {
+      $('.toggleNav').click(function() {
+        $('#flex-nav ul').toggleClass('open');
+      });
+  });
+
+
+
 
 
 $(document).ready(function() { 
@@ -96,4 +105,27 @@ $(document).ready(function() {
       $("#message").removeClass("form-error");
     }
   });
+});
+$(document).ready(function() { 
+  var id = '#dialog';
+  var maskHeight = $(document).height();
+  var maskWidth = $(window).width();
+  $('#mask').css({'width':maskWidth,'height':maskHeight}); 
+  $('#mask').fadeIn(500); 
+  $('#mask').fadeTo("slow",0.9); 
+        var winH = $(window).height();
+  var winW = $(window).width();
+        $(id).css('top',  winH/2-$(id).height()/2);
+  $(id).css('left', winW/2-$(id).width()/2);
+     $(id).fadeIn(2000);  
+     $('.window .close').click(function (e) {
+  e.preventDefault();
+  $('#mask').hide();
+  $('.window').hide();
+     });  
+     $('#mask').click(function () {
+  $(this).hide();
+  $('.window').hide();
+ });  
+ 
 });
